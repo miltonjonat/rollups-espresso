be72ba948029252284e7ee77a2f4b987d1b397bb
Milton Jonathan <milton.jonathan@cartesi.io>
2023-12-18 18:40:20 -0300

    Switch from Cortado to temporary Espresso network
    - Cortado had an issue and was deactivated by the Espresso team
    - A temporary replacement was set up at https://espresso.tspre.org/

--- dehashing-server/src/index.ts
+++ dehashing-server/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 const port = 5006;
 
-const ESPRESSO_BASE_URL = "https://query.cortado.espresso.network";
+const ESPRESSO_BASE_URL = "https://espresso.tspre.org";
 
 app.get('/espresso/:id', async (req, res) => {
   console.log(`Received request for [namespace='espresso', id='${req.params.id}']`)
@@ -15,7 +15,7 @@ app.get('/espresso/:id', async (req, res) => {
 
     // TODO: should filter by VM ID instead of picking up the first one
     // - read dapp deployment file path (dapp.json) from env and extract VM ID from dapp address
-    const payload = data?.block?.transaction_nmt[0]?.payload;
+    const payload = data?.payload?.transaction_nmt[0]?.payload;
     if (payload) {
       let payloadHex = `0x${Buffer.from(payload).toString("hex")}`
       console.log(`Returning payload: '${payloadHex}'`);
